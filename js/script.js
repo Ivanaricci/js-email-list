f8e37c6ea3710f1027447045f28811fc30244c34
Ivanaricci <ivana.ricci26@gmail.com>
2025-04-03 16:27:26 +0200

    [UPD] Done la chiamata ajax

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 // Recuper gli elementi del DOM
 const emailList = document.getElementById("email-list");
+
+// dichiaro le variabili
 const emails = []
+const endpoint = ` https://flynn.boolean.careers/exercises/api/random/mail`
+
+// svolgo la chiamata ajax per interrogare l'endpoint
+axios.get(endpoint).then(res => {
+    const email = res.data.response
+    console.log(email)
+})
