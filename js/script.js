42cbd4a7e441fe4979524a1da42e6b919dbc20a9
Ivanaricci <ivana.ricci26@gmail.com>
2025-04-03 17:46:51 +0200

    [UPD] Done new email with button

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
 // Recuper gli elementi del DOM
 const emailList = document.getElementById("email-list");
-const button = document.getElementById('button')
+const button = document.querySelector('button')
 // dichiaro le variabili
 const emails = []
 const endpoint = ` https://flynn.boolean.careers/exercises/api/random/mail`
+
+function newemail(){
+    emailList.innerHTML = "";
 
 
 // ciclo per ottenere 10 mail
 // svolgo la chiamata ajax per interrogare l'endpoint
 
-for (let i=0; i<=10; i++){
-    axios.get(endpoint).then(res => {
-        const email = res.data.response
-        console.log(email)
-        emails.push(email)
+    for (let i=0; i<=10; i++){
+        axios.get(endpoint).then(res => {
+            const email = res.data.response
+            console.log(email)
+            emails.push(email)
 
 
-        const li = document.createElement('li');
-        console.log(li)
-        li.textContent = email;
-        emailList.appendChild(li)
-    })
+            const li = document.createElement('li');
+            console.log(li)
+            li.textContent = email;
+            emailList.appendChild(li)
+        })
+    }
 }
 
+newemail();
+
+button.addEventListener("click", function(){
+    newemail();
+})
