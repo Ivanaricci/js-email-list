2dda4cbe05baefe6a23417642f3f8a7242e66a6f
Ivanaricci <ivana.ricci26@gmail.com>
2025-04-03 16:40:29 +0200

    [UPD] Done 10 email

--- js/script.js
+++ js/script.js
@@ -5,8 +5,14 @@ const emailList = document.getElementById("email-list");
 const emails = []
 const endpoint = ` https://flynn.boolean.careers/exercises/api/random/mail`
 
+
+// ciclo per ottenere 10 mail
 // svolgo la chiamata ajax per interrogare l'endpoint
-axios.get(endpoint).then(res => {
-    const email = res.data.response
-    console.log(email)
-})
+
+for (let i=0; i<=10; i++){
+    axios.get(endpoint).then(res => {
+        const email = res.data.response
+        console.log(email)
+
+    })
+}
